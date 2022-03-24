aa967cabb24830e24a250a9bdf2eef56f98bd72a
Ibrahim Olanrewaju <semslam007@gmail.com>
2022-03-24 05:09:44 +0100

    initialize the server and router

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const app = require("./app")
+
+app.get("/", (req, res) => {
+    res.status(200).send({ message: "Welcome to restaurant finder API" })
+   
+  });
+  
+  
+  app.all('*', (req, res) => {
+    res.status(404).send({
+        message: "Not Found"
+        })
+  });
+  
+  const PORT =  6000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
